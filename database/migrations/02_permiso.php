55f08a21f9d540c663de052c5435e5ab5763b661
alejandrodiaz0401-hash <alejandrodiaz.0401@gmail.com>
2026-08-25 21:42:43 -0500

    fea\Rol-Permiso

---
+++ database/migrations/02_permiso.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('permisos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 100)->unique();
+            $table->string('descripcion', 200)->nullable();
+            $table->string('estado', 20);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('permisos');
+    }
+};
